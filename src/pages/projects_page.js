c31b97111ec9140d86f54087a3a3a9fabfd50e45
Talha-Bayansar <talha.bayansar@hotmail.com>
2021-01-02 20:30:41 +0100

    added images

--- src/pages/projects_page.js
+++ src/pages/projects_page.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import Card from "../components/card";
 import babili from "../images/babili.png";
+import menuCard from "../images/menuCard.png";
+import webshop from "../images/webshop.png";
 
 const StyledProjectsPage = styled.div`
   display: flex;
@@ -23,6 +25,8 @@ function ProjectsPage() {
       <h1 className="projectPage_title">Projects</h1>
       <div className="projectPage_projects">
         <Card url={babili} />
+        <Card url={menuCard} />
+        <Card url={webshop} />
       </div>
     </StyledProjectsPage>
   );
