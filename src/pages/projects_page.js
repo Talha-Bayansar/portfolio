e1febdd95cf98726692c23e9cb1ae486f8814313
Talha-Bayansar <talha.bayansar@hotmail.com>
2021-01-02 21:26:48 +0100

    added concept data

--- src/pages/projects_page.js
+++ src/pages/projects_page.js
@@ -9,7 +9,7 @@ const StyledProjectsPage = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #c9c9c9;
+  background-color: rgba(200, 200, 200, 0.5);
   padding: 3rem;
 
   & > .projectPage_projects {
@@ -21,14 +21,47 @@ const StyledProjectsPage = styled.div`
   }
 `;
 
+const projects = [
+  {
+    imgUrl: babili,
+    url: "https://babiliproject.herokuapp.com/",
+    date: "June 2020",
+    title: "Babili",
+    description:
+      "Babili is a platform for foreign civilians to learn Dutch by completing excercises given by the teacher.",
+  },
+  {
+    imgUrl: menuCard,
+    url: "https://talha-bayansar.github.io/menucard",
+    date: "October 2020",
+    title: "Menu Card",
+    description:
+      "Menu Card is a project made as an excercise for school. It is a menu card where you have to scan a table so you can make orders based on the menu.",
+  },
+  {
+    imgUrl: webshop,
+    url: "https://talha-bayansar.github.io/EindOpdracht",
+    date: "November 2020",
+    title: "Web Shop",
+    description:
+      "Web Shop is a project made as an assignment for school. This is a concept project where you can sell and buy products with authentication.",
+  },
+];
+
 function ProjectsPage() {
   return (
     <StyledProjectsPage>
       <h1 className="projectPage_title">Projects</h1>
       <div className="projectPage_projects">
-        <Card imgUrl={babili} />
-        <Card imgUrl={menuCard} />
-        <Card imgUrl={webshop} />
+        {projects.map((p) => (
+          <Card
+            imgUrl={p.imgUrl}
+            url={p.url}
+            date={p.date}
+            title={p.title}
+            description={p.description}
+          />
+        ))}
       </div>
     </StyledProjectsPage>
   );
