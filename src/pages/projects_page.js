8743984be2710132d72cdb5230ad5304e7d2eec0
Talha-Bayansar <talha.bayansar@hotmail.com>
2021-03-17 19:43:17 +0100

    updated projects

--- src/pages/projects_page.js
+++ src/pages/projects_page.js
@@ -3,68 +3,77 @@ import styled from "styled-components";
 import Card from "../components/card";
 import babili from "../images/babili.png";
 import rekenbuddy from "../images/rekenbuddy.png";
-import webshop from "../images/webshop.png";
+import memories from "../images/memories.png";
+import planner from "../images/planner.png";
 
 const StyledProjectsPage = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: rgba(200, 200, 200, 0.5);
-  padding: 3rem;
-
-  & > .projectPage_projects {
     display: flex;
-    justify-content: space-evenly;
+    flex-direction: column;
     align-items: center;
-    flex-wrap: wrap;
-    margin: 3rem 0 0;
-  }
+    background-color: rgba(200, 200, 200, 0.5);
+    padding: 3rem;
+
+    & > .projectPage_projects {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        flex-wrap: wrap;
+        margin: 3rem 0 0;
+    }
 `;
 
 const projects = [
-  {
-    imgUrl: babili,
-    url: "https://babiliproject.herokuapp.com/",
-    date: "June 2020",
-    title: "Babili",
-    description:
-      "Babili is a platform for foreign civilians to learn Dutch by completing excercises given by the teacher.",
-  },
-  {
-    imgUrl: rekenbuddy,
-    url: "https://testrekenbuddyleerkracht.web.app/",
-    date: "January 2021",
-    title: "Reken Buddy",
-    description:
-      "Reken Buddy is a platform for students to make math excercises given by their teachers.",
-  },
-  {
-    imgUrl: webshop,
-    url: "https://talha-bayansar.github.io/EindOpdracht",
-    date: "November 2020",
-    title: "Web Shop",
-    description:
-      "Web Shop is a project made as an assignment for school. This is a concept project where you can sell and buy products with authentication.",
-  },
+    {
+        imgUrl: babili,
+        url: "https://babiliproject.herokuapp.com/",
+        date: "June 2020",
+        title: "Babili",
+        description:
+            "Babili is a platform for foreign civilians to learn Dutch by completing excercises given by the teacher. This was made with Java Spring and Thymeleaf.",
+    },
+    {
+        imgUrl: rekenbuddy,
+        url: "https://testrekenbuddyleerkracht.web.app/",
+        date: "January 2021",
+        title: "Reken Buddy",
+        description:
+            "Reken Buddy is a platform for students to make math excercises given by their teachers. This was made with React on the frontend and Java (Spring) with Postgresql on the backend.",
+    },
+    {
+        imgUrl: memories,
+        url: "https://talha-bayansar.github.io/memories-frontend/",
+        date: "january 2021",
+        title: "Memories",
+        description:
+            "A pet project made for fun with React.js on the frontend and Node.js with MongoDB on the backend.",
+    },
+    {
+        imgUrl: planner,
+        url: "https://todo-nextjs-talha-bayansar.vercel.app/",
+        date: "february 2021",
+        title: "Planner",
+        description:
+            "A pet project to use as a planner to organize my work made with Next.js and firebase.",
+    },
 ];
 
 function ProjectsPage() {
-  return (
-    <StyledProjectsPage id="myWork">
-      <h1 className="projectPage_title">Projects</h1>
-      <div className="projectPage_projects">
-        {projects.map((p) => (
-          <Card
-            imgUrl={p.imgUrl}
-            url={p.url}
-            date={p.date}
-            title={p.title}
-            description={p.description}
-          />
-        ))}
-      </div>
-    </StyledProjectsPage>
-  );
+    return (
+        <StyledProjectsPage id="myWork">
+            <h1 className="projectPage_title">Projects</h1>
+            <div className="projectPage_projects">
+                {projects.map((p) => (
+                    <Card
+                        imgUrl={p.imgUrl}
+                        url={p.url}
+                        date={p.date}
+                        title={p.title}
+                        description={p.description}
+                    />
+                ))}
+            </div>
+        </StyledProjectsPage>
+    );
 }
 
 export default ProjectsPage;
