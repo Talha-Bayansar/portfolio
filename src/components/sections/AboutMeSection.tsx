c91a3f6e91f2b9be3fb2f8b455e4c35fa398956a
Talha Bayansar <talha.bayansar@hotmail.com>
2024-04-15 01:33:01 +0200

    escapinng ' character in about me section

--- src/components/sections/AboutMeSection.tsx
+++ src/components/sections/AboutMeSection.tsx
@@ -9,21 +9,21 @@ export const AboutMeSection = () => {
       className="flex flex-col gap-2 text-sm md:text-base"
     >
       <p>
-        I'm a frontend developer with a strong focus on creating elegant and
-        efficient user interfaces. I have a deep appreciation for React and its
-        ecosystem, and I specialize in leveraging technologies like Next.js for
-        robust full-stack development.
+        I&apos;m a frontend developer with a strong focus on creating elegant
+        and efficient user interfaces. I have a deep appreciation for React and
+        its ecosystem, and I specialize in leveraging technologies like Next.js
+        for robust full-stack development.
       </p>
       <p>
-        When I'm not coding, you can find me on the football field ⚽️, enjoying
-        the beautiful game, or spending quality time with my family. I'm happily
-        married, 23 years old, and proud to call 🇧🇪 Belgium my home, with 🇹🇷
-        Turkish heritage shaping my cultural background.
+        When I&apos;m not coding, you can find me on the football field ⚽️,
+        enjoying the beautiful game, or spending quality time with my family.
+        I&apos;m happily married, 23 years old, and proud to call 🇧🇪 Belgium my
+        home, with 🇹🇷 Turkish heritage shaping my cultural background.
       </p>
       <p>
-        I'm passionate about continuous learning and growth, both personally and
-        professionally 📚, and I'm always excited to tackle new challenges and
-        contribute positively to projects and teams.
+        I&apos;m passionate about continuous learning and growth, both
+        personally and professionally 📚, and I&apos;m always excited to tackle
+        new challenges and contribute positively to projects and teams.
       </p>
     </Section>
   );
